051bc310102b0890dc0125d553d5ecb5a37459b0
VelvetBucket <cristian.manrique@pucp.edu.pe>
2025-04-15 13:38:27 -0500

    adding shooting

---
+++ Assets/Scripts/Player/PlayerAimAndShoot.cs
@@ -0,0 +1,39 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAimAndShoot : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _gun;
+    [SerializeField] private GameObject _bullet;
+    [SerializeField] private Transform _bulletSpawnPoint;
+
+    private GameObject _bulletInst;
+
+    private Vector2 _worldMousePosition;
+    private Vector2 _direction;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GunRotation();
+        GunShooting();
+    }
+
+    private void GunRotation()
+    {
+        //Rotate the gun towards the mouse position
+        _worldMousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        _direction = (_worldMousePosition - (Vector2)_gun.transform.position).normalized;
+        _gun.transform.right = _direction;
+    }
+
+    private void GunShooting()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            _bulletInst = Instantiate(_bullet, _bulletSpawnPoint.position, _gun.transform.rotation);
+        }    
+    }
+}

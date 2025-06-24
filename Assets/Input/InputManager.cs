385c5c7df19e0022f740e0d8dacee2f6f4ba66dc
Cam1703 <a20180103@pucp.edu.pe>
2025-06-23 20:13:04 -0500

    Dash

--- Assets/Input/InputManager.cs
+++ Assets/Input/InputManager.cs
@@ -6,6 +6,7 @@ public class InputManager : MonoBehaviour
 
     public static Vector2 movement;
     public static bool attack;
+    public static bool dash;
 
     public static bool menuOpenInput;
     public static bool menuCloseInput;
@@ -13,6 +14,7 @@ public class InputManager : MonoBehaviour
     public static PlayerInput playerInput;
     private InputAction _moveAction;
     private InputAction _attackAction;
+    private InputAction _dashAction;
 
     private InputAction _menuOpenAction;
     private InputAction _menuCloseAction;
@@ -25,12 +27,14 @@ public class InputManager : MonoBehaviour
 
         _menuOpenAction = playerInput.actions["MenuOPEN"];
         _menuCloseAction = playerInput.actions["MenuCLOSE"];
+        _dashAction = playerInput.actions["Dash"];
     }
 
     private void Update()
     {
         movement = _moveAction.ReadValue<Vector2>();
         attack = _attackAction.WasPerformedThisFrame();
+        dash = _dashAction.WasPerformedThisFrame();
 
         menuOpenInput = _menuOpenAction.WasPerformedThisFrame();
         menuCloseInput = _menuCloseAction.WasPerformedThisFrame();
